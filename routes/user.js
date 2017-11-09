44ea25c1f2564fcd4eaad541c277eb2a6fb70fca
caleoasin312 <zikisacalesogaxa@gmail.com>
2017-11-09 08:54:28 +0200

    Remove authentication for user role;

--- routes/user.js
+++ routes/user.js
@@ -4,14 +4,10 @@ var router = express.Router();
 
 router.get('/:username', (req, res) => {
     const username = req.params.username;
-    if (req.session.role === 'waiter') {
-        res.render('user', {
-            username: username,
-            title: 'Waiter'
-        });
-    } else {
-        res.send('Access denied...');
-    }
+    res.render('user', {
+        username: username,
+        title: 'Waiter'
+    });
 });
 
 router.post('/:username', (req, res) => {
